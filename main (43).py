ea4040af601add6e63b2eff020a1b335ee1c9971
PRIYANSHU HAJELA <105854357+priyanshuhajel@users.noreply.github.com>
2022-05-31 15:20:05 +0530

    Add files via upload

---
+++ main (43).py
@@ -0,0 +1,13 @@
+import random 
+print("Create a list of random integers:")
+population = range(0, 100)
+nums_list = random.sample(population, 10)
+print(nums_list)
+no_elements = 4
+print("\nRandomly select",no_elements,"multiple items from the said list:")
+result_elements = random.sample(nums_list, no_elements)
+print(result_elements)
+no_elements = 8
+print("\nRandomly select",no_elements,"multiple items from the said list:")
+result_elements = random.sample(nums_list, no_elements)
+print(result_elements)
