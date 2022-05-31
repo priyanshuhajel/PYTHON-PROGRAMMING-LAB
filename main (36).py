dc07c1b66db263fcbecf874053a1f780c5d2111a
PRIYANSHU HAJELA <105854357+priyanshuhajel@users.noreply.github.com>
2022-05-31 15:19:18 +0530

    Add files via upload

---
+++ main (36).py
@@ -0,0 +1,10 @@
+def func_compute(n):
+ return lambda x : x * n
+result = func_compute(2)
+print("Double the number of 15 =", result(15))
+result = func_compute(3)
+print("Triple the number of 15 =", result(15))
+result = func_compute(4)
+print("Quadruple the number of 15 =", result(15))
+result = func_compute(5)
+print("Quintuple the number 15 =", result(15))
